58525b39aa5611c34107d498c6e2ecacb0530b85
KIDJourney <kingdeadfish@qq.com>
2016-01-22 18:12:48 +0800

    add spamword and logging

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from eventloop import mainLoop
 from common import config_reader
 import configparser
+import logging
 
 if __name__ == "__main__":
     cookie, tiebaName = config_reader()
+    logging.basicConfig(filename='log.txt',level = logging.DEBUG , format='%(asctime)s %(message)s')
     while True:
         mainLoop(tiebaName, cookie)
