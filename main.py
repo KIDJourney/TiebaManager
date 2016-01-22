c84a91395c34549ed50895dc85128e206c4e8257
KIDJourney <kingdeadfish@qq.com>
2016-01-22 10:48:56 +0800

    read from configure file

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from common import config_reader
 import configparser
 
 if __name__ == "__main__":
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-
     cookie, tiebaName = config_reader()
-
     while True:
         mainLoop(tiebaName, cookie)
