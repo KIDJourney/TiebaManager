3b637a82f7ee784424b39c55719895c11b696ba7
KIDJourney <kingdeadfish@qq.com>
2016-01-24 12:02:52 +0800

    follow python standard

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from eventloop import mainLoop
+from eventloop import mainloop
 from common import config_reader
 import configparser
 import logging
 
 if __name__ == "__main__":
     cookie, tiebaName = config_reader()
-    logging.basicConfig(filename='log.txt',level = logging.DEBUG , format='%(asctime)s %(message)s')
+    logging.basicConfig(filename='log.txt', level=logging.DEBUG, format='%(asctime)s %(message)s')
     while True:
-        mainLoop(tiebaName, cookie)
+        mainloop(tiebaName, cookie)
