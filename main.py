66fb95bc3429127a0b4c4f0f47ac3df729a04c76
KIDJourney <kingdeadfish@qq.com>
2016-01-24 21:53:59 +0800

    add exception handler

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from eventloop import mainloop
 from common import config_reader
-import configparser
 import logging
 
 if __name__ == "__main__":
     cookie, tiebaName = config_reader()
     logging.basicConfig(filename='log.txt', level=logging.DEBUG, format='%(asctime)s %(message)s')
     while True:
-        mainloop(tiebaName, cookie)
+        try:
+            mainloop(tiebaName, cookie)
+        except Exception as e:
+            logging.warn(str(e))
