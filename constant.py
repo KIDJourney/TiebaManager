3c1d49ec54a1892caf58badda3e2efbc1ddade83
KIDJourney <kingdeadfish@qq.com>
2016-01-25 17:49:47 +0800

    add constant value to constant file

--- constant.py
+++ constant.py
@@ -1,1 +1,3 @@
-TIEBA_BASE_URL = "http://tieba.baidu.com/mo/m?kw={tieba_name}"
+TIEBA_MOBILE_BASE_URL = "http://tieba.baidu.com/mo/m?kw={tieba_name}"
+TIEBA_MOBILE_URL = "http://tieba.baidu.com/mo/"
+TIEBA_URL = "http://tieba.baidu.com"
