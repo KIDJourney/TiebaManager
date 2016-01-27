6ab75b70699318067b2a5d01f9e1498b0f722e43
KIDJourney <kingdeadfish@qq.com>
2016-01-28 00:01:55 +0800

    add tencent emotion analyze

--- judgemethods.py
+++ judgemethods.py
@@ -1,8 +1,13 @@
+import requests
+import json
+import time
+
 class JudgeBase:
     """
     JudgeMethod base class , implement judge method
     All of subclass only accept two parameters
     """
+
     def judge(self, post):
         pass
 
@@ -11,6 +16,7 @@ class WordsBag(JudgeBase):
     """
     Read word from file and detect if word is in post
     """
+
     def judge(self, post):
         wordbag = []
         with open('wordsbag.txt') as f:
@@ -22,6 +28,28 @@ class WordsBag(JudgeBase):
             if word in content:
                 return True
         return False
+
+
+class TxnlpTextJudge(JudgeBase):
+    """
+    Text emotion analyze provided by Tencent
+    Page:http://nlp.qq.com/semantic.cgi#page4
+    """
+
+    def judge(self, post):
+        url = "http://nlp.qq.com/public/wenzhi/api/common_api.php"
+        body = {'url_path': 'http://10.209.0.215:55000/text/sentiment',
+                'body_data': ""}
+
+        content = json.dumps({'content': post.get_title() + post.get_content()})
+        body['body_data'] = content
+
+        response = requests.post(url, data=body).json()
+
+        time.sleep(1)
+
+        return response['negative'] > 0.75
+
 
 if __name__ == "__main__":
     title = '求送'
