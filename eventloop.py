ee5a122fe4c3a8994f89c58aae4af5830c97075f
KIDJourney <kingdeadfish@qq.com>
2016-02-26 20:17:11 +0800

    refactor with class

--- eventloop.py
+++ eventloop.py
@@ -10,9 +10,9 @@ from common import config_reader
 
 class EventLoop:
     def __init__(self, tieba_name='steam', cookie=None):
-        self.tieba_name = 'steam'
+        self.tieba_name = tieba_name
         self.tieba_crawler = crawler.TiebaCrawler(tieba_name, cookie)
-        self.tieba_judger = judger.Judger([judgemethods.TxnlpTextJudge()])
+        self.tieba_judger = judger.Judger([judgemethods.TestJudge()])
         self.tieba_lawman = lawman.Lawman(tieba_name, cookie)
 
     def loop(self):
@@ -27,7 +27,7 @@ class EventLoop:
                 self.tieba_lawman.delete_post(post.get_del_url())
                 logging.info("{0} delete success".format(post.get_title()))
                 delete_count += 1
-        logging.info("Judging finish , {0} tasks judged , {0} tasks delete".format(len(post_list), delete_count))
+        logging.info("Judging finish , {0} tasks judged , {1} tasks delete".format(len(post_list), delete_count))
 
         logging.info("Loop finish")
 
@@ -36,25 +36,8 @@ class EventLoop:
     def sleep(self):
         time.sleep(random.randint(20, 30))
 
-
-def mainloop(tieba_name='steam', cookie=None):
-    tieba_crawler = crawler.TiebaCrawler(tieba_name, cookie)
-    tieba_judger = judger.Judger([judgemethods.TxnlpTextJudge()])
-    tieba_lawman = lawman.Lawman(tieba_name, cookie)
-
-    logging.info('Starting crawling')
-    post_list = tieba_crawler.get_posts()
-
-    for post in post_list:
-        if tieba_judger.judge(post):
-            tieba_lawman.delete_post(post.get_del_url())
-
-    logging.info("All judge finished")
-
-    time.sleep(random.randint(20, 30))
-
-
 if __name__ == "__main__":
     logging.basicConfig(filename='log.txt', level=logging.DEBUG, format='%(asctime)s %(message)s')
     user_cookie, tieba_name = config_reader()
-    mainloop('dota2提问', user_cookie)
+    loop = EventLoop('dota2提问', user_cookie)
+    loop.loop()
