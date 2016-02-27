def8f52e89cc8a2a2097f3cf305afbec1433693b
KIDJourney <kingdeadfish@qq.com>
2016-02-27 18:15:20 +0800

    modify loggin method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from eventloop import EventLoop
 from common import config_reader
 import logging
+import sys, os
 
 if __name__ == "__main__":
     cookie, tiebaName = config_reader()
@@ -10,4 +11,7 @@ if __name__ == "__main__":
         try:
             eventLoop.loop()
         except Exception as e:
-            logging.warning(str(e))
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+
+            logging.warning("Error :{0} of {1}  on {2}:{3} ".format(str(e), exc_type, fname, exc_tb.tb_lineno))
