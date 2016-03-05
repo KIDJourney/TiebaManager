daea16c0d9a678f2402d43fc1fe101c2e5d3ccf5
KIDJourney <kingdeadfish@qq.com>
2016-03-05 20:33:02 +0800

    add reply judge

--- judger.py
+++ judger.py
@@ -7,26 +7,45 @@ class Judger:
     Classify a post into legal or illegal
     """
 
-    def __init__(self, methods=None):
+    def __init__(self, post_method, reply_method):
         """Accept a list of judgemethod
-        :param Iterable methods:
+        :param Iterable post_method:
         """
-        if not isinstance(methods, collections.Iterable):
-            raise Exception("The judge method must be iterable")
-        if methods is None:
-            raise Exception("The judge method can't be empty")
+        if not isinstance(post_method, collections.Iterable):
+            raise Exception("The post judge method must be iterable")
+        if not isinstance(reply_method , collections.Iterable):
+            raise Exception("The reply judge method must be iterable")
 
-        self.methods = methods
+        self.post_methods = post_method
+        self.reply_methods = reply_method
 
-    def judge(self, postobject):
-        """Judge the post with all method the judge have.
+    def post_judge(self, postobject):
+        """Judge the post with all post judge method the judger have.
         :param Post postobject:
         :return boolean:
         """
-        for method in self.methods:
+        for method in self.post_methods:
             if method(postobject):
                 return True
         return False
+
+    def reply_judge(self, reply_object):
+        """Judge the reply with all reply judge method the judger have.
+        :param reply_object:
+        :return boolean:
+        """
+        for method in self.reply_methods :
+            if method(reply_object) :
+                return True
+        return False
+
+    def __judge(self, postobject):
+        """
+        Judge the post and post reply with all methods the judger have
+        :param postobject:
+        :return:
+        """
+        pass
 
     def add_method(self, method):
         """Add method to the instance
@@ -35,4 +54,4 @@ class Judger:
         """
         if not callable(method):
             raise Exception("The method you add must implement JudgeBase")
-        self.methods.append(method)
+        self.post_methods.append(method)
