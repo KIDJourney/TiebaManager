ee4e6bb7c598eb30aa20183515c55a2beff8e38b
KIDJourney <kingdeadfish@qq.com>
2016-03-21 20:40:00 +0800

    add method reload , refacet logging part

--- src/eventloop.py
+++ src/eventloop.py
@@ -4,12 +4,19 @@ import lawman
 import time
 import random
 import logging
-import judgemethods
+import importlib
 from common import config_reader
+
+try:
+    import judgemethods
+except Exception as e:
+    logging.error("Judge methods error !")
+    raise e
 
 
 class EventLoop:
-    """Main process of the work.
+    """
+    Main process of the work.
     """
 
     def __init__(self, tieba_name='steam', cookie=None):
@@ -19,11 +26,15 @@ class EventLoop:
         self.tieba_lawman = lawman.Lawman(tieba_name, cookie)
 
     def loop(self):
-        """Main Loop of the work :
+        """
+        Main Loop of the work :
         Work flow :
         Crawling Data (TiebaCrawler) -> Generate Post Objects (Post) -> Judge with info (Judger) -> Operator with post (Lawman)
         :return :
         """
+        logging.info("New loop begin")
+        self.reload_judge_method()
+
         logging.info('Crawling start : {0}'.format(self.tieba_name))
         post_list = self.tieba_crawler.get_posts()
         logging.info("Crawling finish : {0}".format(self.tieba_name))
@@ -31,14 +42,39 @@ class EventLoop:
         logging.info('Judging Start , {0} tasks in queue'.format(len(post_list)))
 
         post_delete_count, reply_delete_count, reply_count = self.judge(post_list)
+        post_count = len(post_list)
 
-        logging.info(
-            "Judging finish , {0} post(s) judged , {1} post(s) delete , {2} reply(s) judged , {3} reply(s) delete.".format(
-                len(post_list), post_delete_count, reply_count, reply_delete_count))
-
-        logging.info("Loop finish")
+        self.log_operator(post_count, post_delete_count, reply_count, reply_delete_count)
 
         self.sleep()
+
+    def log_operator(self, post_count, post_delete_count, reply_count, reply_delete_count):
+        """
+        Log the operator happened in this loop
+        :param post_count:
+        :param post_delete_count:
+        :param reply_count:
+        :param reply_delete_count:
+        :return:
+        """
+        if self.tieba_judger.is_post_judge_empty():
+            logging.info("Empty post judge method !")
+        if self.tieba_judger.is_reply_judge_empty():
+            logging.info("Empty replay judge method !")
+        logging.info(
+            "Judging finish , {0} post(s) judged , {1} post(s) delete , {2} reply(s) judged , {3} reply(s) delete.".format(
+                post_count, post_delete_count, reply_count, reply_delete_count))
+        logging.info("Loop finish")
+
+    def reload_judge_method(self):
+        try:
+            logging.info("Trying to reload judge methods")
+            importlib.reload(judgemethods)
+        except Exception as e:
+            logging.error("Reloading judge methods Failed : {0}".format(e))
+            logging.info("Use previous judge methods")
+        else:
+            self.tieba_judger.update_judge_method(judgemethods.POST_METHOD_LIST, judgemethods.REPLY_METHOD_LIST)
 
     def judge(self, post_list):
         post_delete_count = 0
